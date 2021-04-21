167c43fc5a7c6550523ac077931362d9321ff2b9
Neppalli DHEERAJ <ndheeru1992@gmail.com>
2021-04-21 11:26:12 +0530

    Commiting the build

---
+++ build/precache-manifest.98875b0b36e388ff9565ba0fdb746cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043c9b4a3266d8502907db82eb781f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e3086d95859052d48e9",
+    "url": "/static/css/main.b1398a96.chunk.css"
+  },
+  {
+    "revision": "81fc9e14761c90a7bd0d",
+    "url": "/static/js/2.a0691603.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a0691603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3086d95859052d48e9",
+    "url": "/static/js/main.0e299363.chunk.js"
+  },
+  {
+    "revision": "476fec442afe5b76bad1",
+    "url": "/static/js/runtime-main.0e51c17f.js"
+  }
+]);
